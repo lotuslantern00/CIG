f2c1085c84d45dc81314e4a9cda62dbb51ea43e9
lotuslantern00 <ma19006@shibaura-it.ac.jp>
2020-06-05 15:08:19 +0900

    add gui.py

---
+++ gui.py
@@ -0,0 +1,12 @@
+import Tkinter as tk
+
+main_display = tk.Tk()
+
+#window name
+main_display.title("Clock Image Generator")
+
+#window size
+main_display.geometry("400x400")
+
+
+main_display.mainloop()
